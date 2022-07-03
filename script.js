759d79d58a95e6b772cdb6c4bb6af3f6f53f4fd8
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-07-02 23:29:11 -0300

    adequações

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 var btnListRecursos = document.querySelector(".txtlist-recursos");
 
-btnListRecursos.addEventListener("click", toggleRecurso);
-
 function toggleRecurso(){
-    window.alert("Foi");
-    /*var subMenu = document.querySelector(".sub-menu");
-    subMenu.classList.toggle("on");*/
+    var seta = document.querySelector(".seta");
+    var subMenu = document.querySelector(".sub-menu");
+    subMenu.classList.toggle("on");
+    seta.classList.toggle("on");
+}
+function toggleEmpresa(){
+    var seta = document.querySelector(".seta2");
+    var subMenu = document.querySelector(".sub-menu2");
+    subMenu.classList.toggle("on");
+    seta.classList.toggle("on");
 }
 
 function openNav(){
